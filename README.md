11ea5d434b35febf412b0889f2bc199213574dbc
preitinger <peter.reitinger@gmail.com>
2025-05-27 07:56:18 +0200

    refactor optOrUndef, PushData, Version etc.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pr-lib-sw-utils
+
 for data structures and functions common to service worker and client
+
+## Dependencies
+
+        "runtypes": "^7.0.4"
---
+++ sw-utils.ts
@@ -0,0 +1,16 @@
+import * as rt from 'runtypes';
+
+export function optOrUndef<T, U>(runtype: rt.Runtype.Core<T, U>) {
+    return rt.Optional(rt.Undefined.or(runtype));
+}
+
+export const Version = rt.Object({
+    main: rt.Number,
+    sub: rt.Number,
+})
+export type TVersion = rt.Static<typeof Version>
+
+
+export function versionToString(version: TVersion) {
+    return `${version.main}.${version.sub}`;
+}
