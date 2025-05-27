c0e2c7d19eb052e68e80a87a33ad7452ed695a86
preitinger <peter.reitinger@gmail.com>
2025-05-27 12:33:08 +0200

    comments for messages from client to sw

--- messages.ts
+++ messages.ts
@@ -4,17 +4,32 @@
 import * as rt from "runtypes"
 import { Version } from "./sw-utils"
 
+/**
+ * Message from client to its service worker
+ */
 export const GetVersionReq = rt.Object({
     type: rt.Literal('getVersion'),
 })
+/**
+ * Message from client to its service worker
+ */
 export type TGetVersionReq = rt.Static<typeof GetVersionReq>
 
+/**
+ * Response from service worker to its client
+ */
 export const GetVersionRes = rt.Object({
     type: rt.Literal('success'),
     version: Version,
 })
+/**
+ * Response from service worker to its client
+ */
 export type TGetVersionRes = rt.Static<typeof GetVersionRes>
 
+/**
+ * Message from client to its service worker
+ */
 export const SkipWaiting = rt.Object({
     type: rt.Literal('skipWaiting'),
 })
